8366e9bc6064b285359f173a83a8a780873245fa
Landon Dyck <landon@codemonkeysoftware.net>
2020-06-19 14:14:58 -0500

    token env variable is GITLAB_API_TOKEN

--- src/convert.sh
+++ src/convert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TAGS=$(curl --header "Authorization: Bearer $TOKEN" $1 | \
+TAGS=$(curl --header "Authorization: Bearer $GITLAB_API_TOKEN" $1 | \
 jq -c 'map({name: .release.tag_name, date: .commit.created_at, description:.release.description}) | .[] | @base64')
 for row in $TAGS  ; do
   _jq() {
